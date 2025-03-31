089507429a431f6550bdba2c506cbef381f53b1f
Sajith Bandara <sajithbandara0804@gmail.com>
2025-04-01 00:44:10 +0530

    user login function implementation✅

--- controllers/userController.js
+++ controllers/userController.js
@@ -46,7 +46,7 @@ export function loginUser(req,res){
                         phone: user.phone
                     },
                     process.env.JWT_SECRET)
-                    res.json({ message: "Login successful" , token : token});
+                    res.json({ message: "Login successful" , token : token, user : user});
                     
                 } else{
                     res.status(401).json({ error: "Login faild" });
