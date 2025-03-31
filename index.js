280475abb305ae4b723c49cff5cfcbfb37d503df
Sajith Bandara <sajithbandara0804@gmail.com>
2025-04-01 00:11:13 +0530

    cors integration✅

--- index.js
+++ index.js
@@ -7,10 +7,14 @@ import jwt from "jsonwebtoken";
 import dotenv from "dotenv";
 import reviewRouter from "./routes/reviewRouter.js";
 import inquiryRouter from "./routes/inquiryRouter.js";
+import cors from "cors";
 
 dotenv.config();
 
-const app = express()
+const app = express();
+
+app.use(cors());
+    
 
 app.use(bodyParser.json());
 app.use((req,res,next)=>{
